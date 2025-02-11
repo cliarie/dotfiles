efcf0c40e40d1d42a54c40f22c3a7aed65ef143b
Claire <clarrechen@gmail.com>
2025-02-10 21:54:52 -0600

    Add commenting plugin

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -102,4 +102,11 @@ return {
             },
         },
     },
+    {
+        "numToStr/Comment.nvim",
+        lazy = false,
+        config = function()
+            require("Comment").setup()
+        end,
+    },
 }
